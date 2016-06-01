36cb6728666e3e1668e0ed2dc529f9eff0c19f89
Leandro Martinez <leandromartinez98@gmail.com>
2016-06-01 17:42:39 -0300

    Added safeguard for linkdir name.

--- src/topolink.f90
+++ src/topolink.f90
@@ -261,6 +261,10 @@ program topolink
       write(*,*) ' ERROR: Need linkdir if printlinks is yes. '
       stop
     else 
+      i = length(linkdir)
+      if ( linkdir(i:i) /= "/" ) then
+        linkdir(i+1:i+1) = "/"
+      end if
       write(*,*) ' Directory to output links as PDB files: ', trim(adjustl(linkdir))
     end if
   end if
