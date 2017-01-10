75411f9d7548e26edecbed74a534ae72ad12568f
leandro <leandromartinez98@gmail.com>
2017-01-10 13:28:13 +0000

    incorporated strclean into string_operations

--- src/string_operations.f90
+++ src/string_operations.f90
@@ -228,6 +228,20 @@ module string_operations
     
     end function countwords
 
+    !
+    ! Subroutine that removes tab characters from string
+    !
+    
+    subroutine strclean(record)
+    
+      character(len=200) :: record
+      record = trim(adjustl(record))
+      do i = 1, 200
+        if ( record(i:i) == achar(9) ) record(i:i) = achar(32)
+      end do
+    
+    end subroutine strclean
+
 end module string_operations
 
 
