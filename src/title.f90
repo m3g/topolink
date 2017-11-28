5101c7f18666eac97d32e329be11a97e3378805b
Leandro Martinez <leandro@iqm.unicamp.br>
2017-11-28 10:02:18 -0200

    Changed version file to 17.332

--- src/title.f90
+++ src/title.f90
@@ -22,7 +22,7 @@ subroutine title()
   write(*,"(t3,56('#'),' TOPOLINK ',55('#'))") 
   write(*,"(t15,a)") 'L. Martinez, Institute of Chemistry - University of Campinas. http://leandro.iqm.unicamp.br'
   write(*,*) 
-  write(*,"(t105,a)") " Version 17.331 "
+  write(*,"(t105,a)") " Version 17.332 "
   write(*,*) 
 
 end subroutine title
