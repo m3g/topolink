18339ae49fd75b6671caf394ad0f8f7ae7c14cf0
Leandro Martinez <leandro@iqm.unicamp.br>
2018-10-11 16:46:51 -0300

    fixed space in logs to back server parser compatibility

--- src/writelog.f90
+++ src/writelog.f90
@@ -5,10 +5,10 @@ subroutine writelog(string)
 
   character(len=*) :: string
 
-  write(*,*) trim(adjustl(string))
+  write(*,*) " "//trim(adjustl(string))
 
   if ( output_log ) then
-    write(output_log_unit,*) trim(adjustl(string))
+    write(output_log_unit,*) " "//trim(adjustl(string))
   end if
 
   return
