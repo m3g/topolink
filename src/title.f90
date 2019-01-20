638d6ee27de52222f9108ff338f5d5783a62ce4d
Leandro Martinez <leandro@iqm.unicamp.br>
2019-01-20 10:55:34 -0200

    updated title

--- src/title.f90
+++ src/title.f90
@@ -6,13 +6,6 @@
 ! http://leandro.iqm.unicamp.br
 ! Out 5, 2015
 !
-! Reference:
-!
-! L. Martinez, A. Ferrari, F. C. Gozzo,
-! TopoLink: A package to compute the likelihood of structural models
-! based on surface accessible topological distances
-! 2015
-!
 
 ! Subroutine that prints the title
 
@@ -21,7 +14,7 @@ subroutine title(iout)
   integer :: iout
   write(iout,*) 
   write(iout,"(t3,56('#'),' TOPOLINK ',55('#'))") 
-  write(iout,"(t15,a)") 'L. Martinez, Institute of Chemistry - University of Campinas. http://leandro.iqm.unicamp.br'
+  write(iout,"(t18,a)") 'Institute of Chemistry - University of Campinas. http://m3g.iqm.unicamp.br/topolink'
   write(iout,*) 
   write(iout,"(t105,a)") " Version 19.020 "
   write(iout,*) 
