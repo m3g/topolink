0418d47d19e6ee41bf601469f566026dc0fd5459
Leandro Martinez <leandro@iqm.unicamp.br>
2019-02-09 10:09:03 -0200

    added linkensemble

--- julia/src/linkensemble.jl
+++ julia/src/linkensemble.jl
@@ -1,28 +1,23 @@
 
 include("./structures.jl")
 
-function linkensemble( logs , cscore :: Cscore )
+function linkensemble(models)
   
-  nlinks = logs[1].nlinks
-  nsatisfied = [ 0 for i in 1:length(logs) ]
+  nlinks = models[1].nlinks
+
+  nsatisfied = [ 0 for i in 1:length(models) ]
+
   satisfied = [ false for i in 1:nlinks ]
 
-  ilog = 0
-  for log in logs
-    ilog = ilog + 1
-    for i in 1:log.nlinks
-      if log.link[i] == log[1].link[i]
-
-        if ! satisfied[log.
-
-        if log.link[i].observed 
-          if log.link[i].result == "OK: FOUND" 
-            nsatisfied[ilog] = nsatisfied
-
-          end
-        end
+  for imodel in 1:length(models)
+    for ilink in 1:nlinks
+      if models[imodel].link[ilink].result == "OK: FOUND"
+        satisfied[ilink] = true
       end
     end
+    nsatisfied[imodel] = count(satisfied)
   end
+
+  return nsatisfied
 
 end
